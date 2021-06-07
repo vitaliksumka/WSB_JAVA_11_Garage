6915b3b98b67169a26fc19e3bee3ebfd4d9e9226
vitalik1652 <vovk2001.ua@gmail.com>
2021-06-07 15:25:48 +0200

    task11

---
+++ zad 11/src/com/company/devices/Car.java
@@ -0,0 +1,51 @@
+package com.company.devices;
+
+import com.company.Human;
+
+public abstract class Car extends Device {
+    private String plates;
+
+    public Car(String producer, String model) {
+        super(producer, model, 10000.0);
+    }
+
+    public String getPlates() {
+        return plates;
+    }
+
+    public void setPlates(String plates) {
+        this.plates = plates;
+    }
+
+    public String toString() {
+        return producer + " " + model + " " + plates;
+    }
+
+    @Override
+    public void turnOn() {
+        System.out.println("Samochód " + this.producer + " " + this.model + " uruchomiony. Możesz jechać!");
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    @Override
+    public void sell(Human seller, Human buyer, Double price) throws Exception {
+        if (buyer.couldBuy(this, price) && seller.hasDevice(this) && buyer.getNumberParkingFreeSpace() != null) {
+            buyer.setCar(buyer.getNumberParkingFreeSpace(), this);
+            seller.removeCar(this);
+            buyer.cash -= price;
+            seller.cash += price;
+            System.out.println("Tranzakcja udana.");
+        } else {
+            System.out.println("Tranzakcja nie powiodła się!");
+        }
+    }
+
+    public abstract void refuel();
+}
